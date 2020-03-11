d6e295b1741bc7cf25e7ba99760ccf3cefdc90fe
fr1t2 <fr1t2@protonmail.com>
2020-03-11 16:28:49 -0600

    formatting response

--- _scripts/discord/cmd/drip.js
+++ _scripts/discord/cmd/drip.js
@@ -131,6 +131,12 @@ module.exports = {
         else if (faucetCheck[0][1].drip_found == false) {
           // no drip found. Do things here.
           console.log('no drips found. Adding to db and sending a drip')
+          message.react(':droplet:')
+                      .then(() => message.react('🇶'))
+                      .then(() => message.react('🇷'))
+                      .then(() => message.react('🇱'))
+                      .catch(() => console.error('One of the emojis failed to react.'));
+                    message.channel.stopTyping(true);
         }
       });
     });
