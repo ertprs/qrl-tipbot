7e8d03fc030bc5bfba791462aef8db9a6b7ed07b
fr1t2 <fr1t2@protonmail.com>
2020-03-11 23:02:30 -0600

    addign drip

--- _scripts/db/dbHelper.js
+++ _scripts/db/dbHelper.js
@@ -391,8 +391,9 @@ async function AddUser(args) {
                 }
                 const future_tip_amount = futureTipped[0].future_tip_amount * 1000000000;
                 resultsArray.push({ future_tip_amount: future_tip_amount });
-                const agreeValues = [ [userID, 0, new Date()] ]
+                const agreeValues = [ [userID, '0', new Date()] ]
                 const agree_default = 'INSERT INTO users_agree(user_id, agree, time_stamp) VALUES ?'
+                console.log('agree_default: ' + agree_default);
                 callmysql.query(futureTips_payout, [agreeValues], function(err, futureTipped) {
                   if (err) {
                     console.log('[mysql error]', err);
