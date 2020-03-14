f30b86fde4307146d2335be0ddcfd5be0269333c
fr1t2 <fr1t2@protonmail.com>
2020-03-14 11:46:51 -0600

    interval to minutes

--- _scripts/discord/cmd/drip.js
+++ _scripts/discord/cmd/drip.js
@@ -156,7 +156,7 @@ module.exports = {
         console.log('faucetCheck results' + JSON.stringify(faucetCheck));
         if (faucetCheck[0][1].drip_found == true) {
           console.log('user has been found recently, no drips');
-          message.reply('You have received a tip recently :no_entry_sign: \nPlease come back in  ***' + config.faucet.payout_interval + ' hours*** from ***' + faucetCheck[0][0][0].time_stamp + '*** to request more funds.');
+          message.reply('You have received a tip recently :no_entry_sign: \nPlease come back in  ***' + config.faucet.payout_interval + ' minutes*** from ***' + faucetCheck[0][0][0].time_stamp + '*** to request more funds.');
           return;
         }
         else if (faucetCheck[0][1].drip_found == false) {
