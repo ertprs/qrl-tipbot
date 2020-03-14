0c8fc05919b7825e9126adeab92f8615d1521505
fr1t2 <fr1t2@protonmail.com>
2020-03-14 11:33:50 -0600

    Faucet Payout script

---
+++ _scripts/faucet/payout.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3.5
+# ****************************************************************
+#                 FaucetPayout Script
+# 
+# Script reads from the tipbot database to find any valid faucet requests. These
+# requests will be in the DB.faucet_payments table. The request is marked {paid: false} to start
+# Using the user_id from this table to lookup the wallet_pub
+# and add it to a list of addresses to pay.
+# 
+# 
+# 
+# ****************************************************************
+import requests
+import json
+import mysql.connector
+import datetime
+import logging
+from decimal import *
+# logging settings
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.basicConfig(format='%(asctime)s %(message)s', filename='/home/fr1t2/.qrl/faucet.log', level=logging.INFO)
+# load the config file (find it at "data['TOPIC']['SETTING'])"
+with open('../../_config/config.json') as json_data_file:
+    conf = json.load(json_data_file)
+
+# db settings from config file
+host = conf['database']['db_host']
+user = conf['database']['db_user']
+passwd = conf['database']['db_pass']
+database = conf['database']['db_name']
+
+fee = conf['wallet']['tx_fee'] # in shor X/10^9=quanta
+feeShor = (float(fee) * 1000000000)
+current_time = datetime.datetime.now()
+#tx_hash = ''
+
+print(int(feeShor))
+
+# SQL queries
+addressInfo = "SELECT wallets.wallet_pub FROM wallets, faucet_payouts WHERE faucet_payouts.paid = 0 AND faucet_payouts.user_id = wallets.user_id"
+amountInfo = "SELECT faucet_payouts.drip_amt AS drip_amt FROM wallets, faucet_payouts WHERE faucet_payouts.paid = 0 AND faucet_payouts.user_id = wallets.user_id"
+#UpdateSQL = "UPDATE ADMIN SET PAID = 0 WHERE PAID = 1"
+# DB Connection
+mydb = mysql.connector.connect(
+        host = host,
+        user = user,
+        passwd = passwd,
+        database = database
+)
+# interact with the db and mysql.connector with details from above
+mycursor = mydb.cursor()
+# Collect the amount to send
+mycursor.execute(amountInfo)
+
+howMuch = mycursor.fetchall()
+
+amount_toSend = []
+for row in howMuch:
+  userAmt = Decimal(row[0])
+  sendAmt = float(userAmt) * 10**9
+  amount_toSend.append(int(sendAmt))
+
+print(amount_toSend)
+
+# Collect the addresses to send to
+mycursor.execute(addressInfo)
+who = mycursor.fetchall()
+
+addresses_to = []
+for address in who:
+        addresses_to.append(address[0])
+
+print(json.dumps(addresses_to))
+
+print(current_time)
+
+if not who:
+        exit()
+else:
+    master_address = conf['faucet']['faucet_wallet_pub']
+    print(master_address)
+
+def relayTransferTxnBySlave(addresses_to, amounts, feeShor, master_address):
+  print(type(amounts))
+  payload = {'addresses_to': addresses_to, 'amounts': amounts, 'fee': int(feeShor), 'master_address': master_address }
+  print(payload)
+  QRLrequest = requests.post("http://127.0.0.1:5359/api/RelayTransferTxnBySlave", json=payload)
+  response = QRLrequest.text
+  relayTransferTxnBySlaveResp = json.loads(response)
+  jsonResponse = relayTransferTxnBySlaveResp
+  tx_hash = jsonResponse['tx']['transaction_hash']
+  print('tx_hash')
+  print(tx_hash)
+  #logging.info('TX HASH: %s', json.dumps(jsonResponse['tx']['transaction_hash']))
+  logging.info('ADMIN test:\n   amount = %s \n   payees = %s \n   fee = %s\n   masterAddress = %s\n%s ADMIN test:\n', amounts, addresses_to, feeShor, master_address, current_time)
+  #print('ADMIN test:\n   amount = %s \n   payees = %s \n   fee = %s\n   masterAddress = %s\n%s ADMIN test:\n', amounts, addresses_to, feeShor, master_address, current_time)
+  print(f'ADMIN test:\n   amount = {amounts} \n   payees = {addresses_to} \n   fee = {feeShor}\n   masterAddress = {master_address}\n{current_time} ADMIN test:\n')
+  return(jsonResponse)
+
+tx = relayTransferTxnBySlave(addresses_to, amount_toSend, feeShor, master_address)
+
+print(json.dumps(tx))
+
+print(tx['tx']['transaction_hash'])
+
+tx_hash = tx['tx']['transaction_hash']
+UpdateSQL = ("UPDATE faucet_payouts SET faucet_payouts.paid = 1, faucet_payouts.updated_at = '%s', faucet_payouts.tx_hash = '%s' WHERE faucet_payouts.paid = 0" % (current_time, tx_hash))
+print(UpdateSQL)
+
+
+
+
+mycursor.execute(UpdateSQL)
+mydb.commit()
+mydb.close()
+#logging.info('ADMIN test:\n   amount = %s \n   payees = %s \n   fee = %s\n   masterAddress = %s\n%s ADMIN test:\n', amount, payees, fee, masterAddress, current_time)
+exit()
