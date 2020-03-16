97006be8733f57a6bdf8ead150c67f00b6d9630d
fr1t2 <fr1t2@protonmail.com>
2020-03-16 12:27:24 -0600

    Add check if is to bot

--- _scripts/discord/cmd/agree.js
+++ _scripts/discord/cmd/agree.js
@@ -46,7 +46,7 @@ module.exports = {
 
 
 // used to test the function, remove before going live
-        if (message.mentions.users.size > 0) {
+        if (message.mentions.users.size > 0 || message.isMemberMentioned(client.user)) {
           const users_Service_ID = message.mentions.users.first().id;
           const service_ID = '@' + users_Service_ID;
           const GetAllUserInfoPromise = GetAllUserInfo({ service: 'discord', service_id: UUID });
