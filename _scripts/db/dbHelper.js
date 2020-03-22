c02aa787b62eb1d27a00d2da101bce8bfad4aaed
fr1t2 <fr1t2@protonmail.com>
2020-03-22 06:34:49 -0600

    testing

--- _scripts/db/dbHelper.js
+++ _scripts/db/dbHelper.js
@@ -78,7 +78,7 @@ async function GetAllUserInfo(args) {
         }
         const infoResult = JSON.parse(JSON.stringify(user_info_update));
         // const foundRes = { user_found: 'true' };
-        foundResArray.push({ user_found: 'true', infoResult });
+        foundResArray.push({ user_found: 'true', user_info_update });
         //Array.prototype.push.apply(foundResArray, infoResult);
         // console.log('getAllInfoSearch foundResArray ' + JSON.stringify(foundResArray));
         resolve(foundResArray);
