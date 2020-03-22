e3a7053dcd09df2881d1a8e54c512a33f25dba92
fr1t2 <fr1t2@protonmail.com>
2020-03-22 13:40:51 -0600

    Updates for getAllUserInfo changes

--- _scripts/discord/cmd/withdraw.js
+++ _scripts/discord/cmd/withdraw.js
@@ -103,7 +103,7 @@ module.exports = {
 
          console.log('user_id ' + user_id);
          console.log('wallet_pub ' + wallet_pub);
-         console.log('wallet_bal ' + wallet_bal * 10000000);
+         console.log('wallet_bal ' + wallet_bal * 1000000000);
          console.log('shor_bal ' + shor_bal);
          console.log('user_name ' + user_name);
          console.log('transfer_to ' + transfer_to);
