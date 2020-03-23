f7604ea3da30dd3cc5a8a9eff804b7fe6dfb0ccb
fr1t2 <fr1t2@protonmail.com>
2020-03-22 22:31:41 -0600

    Updates for getAllUserInfo changes

--- _scripts/discord/cmd/drip.js
+++ _scripts/discord/cmd/drip.js
@@ -5,7 +5,7 @@ module.exports = {
   aliases: ['faucet', 'freeqrl', 'free', 'drop'],
   guildOnly: false,
   usage: ' ',
-  cooldown: 1440 * 60, // 1440 is one day
+  // cooldown: 86399,
 
   execute(message, args) {
     const Discord = require('discord.js');
