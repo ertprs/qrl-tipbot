4da48a57fdbdb018440aeca4cfb7ba98b0c74ba2
fr1t2 <fr1t2@protonmail.com>
2020-03-31 08:54:14 -0600

    Discord.MessageEmbed

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const health = require('./_test/health/healthcheck');
 const fs = require('fs');
 const chalk = require('chalk');
 const service = '';
-console.log('Starting the ' + chalk.black.bgBlue.bold('QRL TipBot'));
+
+setTimeout(console.log('Starting the ' + chalk.black.bgBlue.bold('QRL TipBot')), 1500);
+
+//console.log('Starting the ' + chalk.black.bgBlue.bold('QRL TipBot'));
 
 // from https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
 const { spawn } = require('child_process');
