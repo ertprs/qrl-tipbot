ddd8db13a59d5e28201b77ca7feddad1d545e3c5
fr1t2 <fr1t2@protonmail.com>
2020-03-31 11:40:23 -0600

    formatting

--- _scripts/discord/index.js
+++ _scripts/discord/index.js
@@ -47,12 +47,12 @@ client.on('ready', () => {
   //console.log('client.cache ' + JSON.stringify(client.channels.cache));
   //console.log('client.guild ' + JSON.stringify(client.guilds.cache));
   console.log(chalk`
-{green ==========================================}
+{cyan ==========================================}
 {cyan Discord TipBot Started at: ${nownow}}
-  {blue {green !} Connected to {red ${client.guilds.cache.size}} guilds }
-  {blue {green !} Connected to {red ${client.users.cache.size}} users } 
-  {blue {green !} Connected to {red ${client.channels.cache.size}} channels }
-{green ==========================================}
+  {blue {cyan !} Connected to {grey ${client.guilds.cache.size}} guilds }
+  {blue {cyan !} Connected to {grey ${client.users.cache.size}} users } 
+  {blue {cyan !} Connected to {grey ${client.channels.cache.size}} channels }
+{cyan ==========================================}
     `);
   client.user.setActivity('Tipping $$ QRL $$');
 });
