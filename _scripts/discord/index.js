c6480718e4666fe27f38c4d8c105abda6c06fe60
fr1t2 <fr1t2@protonmail.com>
2020-03-31 10:41:08 -0600

    client.guilds

--- _scripts/discord/index.js
+++ _scripts/discord/index.js
@@ -42,7 +42,8 @@ const cooldowns = new Discord.Collection();
 // start the bot
 client.on('ready', () => {
   console.log('client.users ' + JSON.stringify(client.users.cache));
-  console.log('client.users ' + JSON.stringify(client.channels.cache));
+  console.log('client.cache ' + JSON.stringify(client.channels.cache));
+  console.log('client.guild ' + JSON.stringify(client.guilds.cache));
   console.log(`Discord Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.size} guilds.`);
   client.user.setActivity('Tipping $$ QRL $$');
 });
