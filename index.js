6e3b79f7f8432e0d2717c0d945d186b0578d7e90
fr1t2 <fr1t2@protonmail.com>
2020-04-03 16:13:47 -0600

    mysq checks

--- index.js
+++ index.js
@@ -94,7 +94,8 @@ BotWalPubQuery()
   const listAddresses = wallet.listAll;
   listAddresses().then(function(addresses) {
     console.log(typeof(addresses));
-    var re = /"\n|"\n"|"\n/; // split on colon space or comma space
+
+    var re = /["\n|"\n"|"\n]+/; // split on colon space or comma space
     var ar = addresses.split(re);
     console.log(ar);
 
