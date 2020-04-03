8f886c4ad5b8ddbf6516d56126dfe9bd35ca72fa
fr1t2 <fr1t2@protonmail.com>
2020-04-03 16:31:57 -0600

    mysq checks

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ BotWalPubQuery()
   const listAddresses = wallet.listAll;
   listAddresses().then(function(addresses) {
     console.log(addresses);
-    const ar = addresses.split();
+    const ar = addresses.split(',');
     console.log(ar);
 
   });
