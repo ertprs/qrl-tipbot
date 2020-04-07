95c0d55cb9ef2f1a3ce6e8e6388f255a066c9897
fr1t2 <fr1t2@protonmail.com>
2020-04-06 22:47:35 -0600

    Updates

--- _scripts/discord/cmd/withdraw.js
+++ _scripts/discord/cmd/withdraw.js
@@ -161,7 +161,7 @@ module.exports = {
             return;
           }
           const trans_amt_shor = trans_amt * toShor;
-          const total_transfer = (trans_amt_shor - fee);
+          const total_transfer = Math.round(trans_amt_shor - fee);
           // console.log('trans_amt: ' + trans_amt + ' trans_amt_shor: ' + trans_amt_shor + ' total_transfer: ' + total_transfer);
           // check if amount is equal or less than bal
           // console.log('transfer Details. trans_amt :' + trans_amt + ' trans_amt_shor: ' + trans_amt_shor + ' total_transfer: ' + total_transfer);
