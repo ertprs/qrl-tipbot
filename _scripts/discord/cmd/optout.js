ccfce84507a12a5f1dae848a4f0f9ebc3ae3e05e
fr1t2 <fr1t2@protonmail.com>
2020-04-07 08:14:24 -0600

    Updates

--- _scripts/discord/cmd/optout.js
+++ _scripts/discord/cmd/optout.js
@@ -113,7 +113,7 @@ module.exports = {
 
                   // console.log('MessageAuthorID: ' + UUID + ' config admin: ' + config.discord.bot_admin);
                   // only allow the discord admin defined in teh config execute this
-                  if (message.mentions.users.size > 0 && UUID == config.discord.bot_admin) {
+                  if (message.mentions.users.size > 0 && UUID == config.discord.bot_admin && (args[0] != 'donate' || args[0] != 'transfer' || args[0] != 'tip')) {
                     const users_Service_ID = message.mentions.users.first().id;
                     const service_ID = '@' + users_Service_ID;
                     console.log('users serviceID mentioned: ' + service_ID);
