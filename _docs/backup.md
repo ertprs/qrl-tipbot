f5b29edc65e8deac67f8af77fa6327b934e23170
fr1t2 <fr1t2@protonmail.com>
2020-05-29 15:20:35 -0600

    Backup details added and developed

--- _docs/backup.md
+++ _docs/backup.md
@@ -13,9 +13,11 @@ We will run the backup frequently, saving the latest data and pushing out to mul
 - Backups are stored on multiple platforms in separate locations
 - Validation of the backup and successful transmission. Notify on failure
 
-## Backup Data
+## Backup System
 
 We need to backup any of the user information and wallet files needed for rebuilding the bot upon failure.
+
+### Backup Data 
 
 | Data | Location | Description | 
 | ---- | ---------| ----------- |
@@ -23,5 +25,86 @@ We need to backup any of the user information and wallet files needed for rebuil
 | Config File | {BOT_DIR}/_config/config.json | The bot main configuration file. |
 | Bot Database | MySQL database | The bot database, containing user info and transaction details (Public keys and amounts) all info is public. |
 | Node State Files | ~/.qrl/data/state | The QRL Blockchain. Will save time on the re-sync however is large. |
+
+
+### Backup Locations
+
+Uploading the synced and tar'd files to multiple locations helps guarantee that things are recoverable
+
+| Service | Setup | Notes | 
+| ------- | ----- | ----------- |
+| Dropbox | Install dropbox to server and link to account | symlink the backup directory to the default dropbox folder, and make sure it is not syncing the entire dropbox account!|
+| Amazon S3 Bucket | Using the node AWS SDK |
+
+
+### Backup Details
+
+The backup system uses Rsync to incrementally update any changes to the files we are interested in, as well as `mysqldump` to get the latest database info from MySQL. This will backup all of these files into a single location in the `/_script/backup/incremental/` directory
+
+The system is run from Cron and the frequency of backup is set in the configuration file `/_config/config.json`
+
+
+#### Amazon
+
+
+#### Dropbox
+
+
+#### NextCloud WebDav
+
+To sync to a NextCloud (NC) server the config file will need to be modified to meet WebDav users information.
+
+> Be cautious of security implications of sharing access keys to the NC server. Configure a new tipbot user to hold these files to limit exposure there.
+
+**Config Setup**
+
+In the `config.backup` section add you user details to connect to the Nextcloud server. 
+
+**Nextcloud Configuration Directives**
+
+| Setting | Information |
+| --- | --- |
+| `config.backup.nextcloud_server` | Server FQDN |
+| `config.backup.nextcloud_user` | NextCloud User Name |
+| `config.backup.nextcloud_pass` | NextCloud User Password *(strong)* |
+
+
+**Backup Procedure**
+
+The system will keep files for a few versions to ensure backup corruption is prevented.
+
+Files will be saved -
+
+- Last Backup taken every hour from TipBot
+- Last 24 hours (24 Backups)
+- Every day for last week (7 Backups)
+- Weekly Backup for last month
+- Monthly Backup
+
+##### Examples
+
+**Bash code**
+
+Using curl to interact with webdav server
+
+```bash
+# Upload file error.log...
+curl -u user:pass -T error.log "https://example.com/nextcloud/remote.php/dav/files/USERNAME/$(date '+%d-%b-%Y')/error.log"
+
+# Move a file
+curl -u user:pass -X MOVE --header 'Destination: https://example.com/nextcloud/remote.php/dav/files/USERNAME/target.jpg' https://example.com/nextcloud/remote.php/dav/files/USERNAME/source.jpg
+
+```
+
+
+**NPM webdav**
+
+> https://www.npmjs.com/package/webdav
+
+`npm install webdav --save`
+
+
+#### Rsync Server
+
 
 
--- _scripts/backup/backup.js
+++ _scripts/backup/backup.js
@@ -1,7 +1,140 @@
+/*
+
+NEED TO ADD PACKAGES TO package -
+
+npm install mysqldump
+
+*/
+
+/*
+NEED TO WRITE
+
+Need a recover script to rebuild the bot!
+
+*/
 
 
+// Script to backup all of the files needed to run the bot
+// This file will accept variables and do things with them
+//  to backup to various services. 
+// By default the script only backs up the files locally.
+// Keep track of when the script was ran last in teh .last_run file located in this directory.
 /*
 var exec = require('child_process').exec;
 var child = exec(' mysqldump -u root -p[root_password] [database_name] > dumpfilename.sql');
-
 */
+
+// const webdav = require('webdav');
+// const aws = require('awsSDK');
+const fs = require('fs');
+const crypto = require('crypto');
+const config = require('../../_config/config.json');
+
+
+/*
+The file /_scripts/backup/.last_run will hold all of the relavant information for
+each file that was backed up, including the sha256sum of the file when created.
+This file will track the last run, last rolled, and allow tracking of when we should do things
+// FEATURE - Add signing functions from the qrl-cli lattice keys and track this in
+	ephemeral communications to a private location. Also signing and encrypting with the
+	lattice keys would add validity to that system and security to this one.
+*/
+const last_run_file_path = '__dirname/.last_run';
+// const last_run = fs.readFileSync(last_run_file_path);
+// const last__run_data = JSON.parse(last_run);
+const mysqldump = require('mysqldump');
+
+// function to update the last_run json file
+// data comes in an array { service: (dropbox, webdav, rsync, s3), sha256sum: sha256sum, file_name: file_name }
+function updateLastRun(data) {
+	const time_now = new Date();
+	const updated_info = {
+		service: data.service,
+		sha256sum: data.sha256sum,
+		file_name: data.file_name,
+		last_updated: time_now,
+	};
+	const update = JSON.stringify(updated_info, null, 2);
+	// check for file? may not need this at all...
+	try {
+		if (fs.existsSync(last_run_file_path)) {
+			// file exists, send the update info
+			fs.writeFileSync(update);
+			console.log('Updated last_run with this data:\n' + update);
+		}
+		else {
+			console.log('no file found at: ' + last_run_file_path);
+		}
+	}
+	catch(err) {
+		console.error(err);
+		}
+}
+
+// sha256sum file. Send the file location to sum
+function sha256sum(file) {
+	// change the algo to sha1, sha256 etc according to your requirements
+	const algo = 'sha256';
+	const shasum = crypto.createHash(algo);
+
+	const s = fs.ReadStream(file);
+	s.on('data', function(d) { shasum.update(d); });
+	s.on('end', function() {
+		const d = shasum.digest('hex');
+		console.log(d);
+		return d;
+	});
+}
+
+// backup database into a tar.gz file and save in the backup folder
+function sqlBackup() {
+	// dump the result straight to a compressed file
+	mysqldump({
+		connection: {
+			host: config.database.db_host,
+			user: config.database.db_user,
+			password: config.database.db_pass,
+			database: config.database.db_name,
+		},
+		dumpToFile: config.backup.location,
+		compressFile: true,
+	});
+}
+
+// Roll files in the backup, moving and removing old backups to manage the size
+function rollFiles() {
+/*
+Keep backups for the last month in various snapshots
+- Monthly backup (first day of the month, Includes the state files)
+- Weekly Backups (Save every Friday of the month @ 00:00)
+- Daily Backups (Save the last week every day @ 00:00)
+- Save the last 24Hrs (Save every hour, on the hour)
+- Write the roll update to thelast_run file
+*/
+
+// set the time now into a variable
+const now = new Date();
+
+now.setDate(now.getDate()-7);
+var mydatestring =   // from the json file, change for each in the file... format should be - '2016-07-26T09:29:05.00';
+var mydate = new Date(mydatestring);
+var difference = now - mydate; // difference in milliseconds
+const TOTAL_MILLISECONDS_IN_A_WEEK = 1000 * 60 * 24 * 7;
+
+if (Math.floor(difference / TOTAL_MILLISECONDS_IN_A_WEEK) >= 7) {
+    console.log("Current date is more than 7 days older than : " + mydatestring);
+}
+
+
+
+
+
+}
+// tar everything
+// Take all files and tar tehm into one condensed file to send in email?
+
+
+// Upload files to webdav
+
+
+// Upload files to s3 bucket
