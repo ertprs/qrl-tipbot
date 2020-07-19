3bc6db4a28aa7cc17acf76ff781d8d928cb9fa7e
fr1t2 <fr1t2@protonmail.com>
2020-07-18 21:47:26 -0600

    Info command formatting

--- _scripts/discord/cmd/info.js
+++ _scripts/discord/cmd/info.js
@@ -17,7 +17,7 @@ module.exports = {
     }, 1000);
   }
 
-  ReplyMessage('QRL TipBot Info -\nTipbot for giving away QRL. Setup an account now with `+add` and start tipping.\n\nIf you would like to contribute and support the bot, donation addresses below, or simply tip the bot.\n Faucet Dontaion Address:\n' + config.faucet.faucet_wallet_pub + '\nTipbot Donation Address:\n' + config.bot_details.bot_donationAddress);
+  ReplyMessage('TipBot Info -\nIf you would like to support the bot, use the donation addresses below, or simply tip the bot.\n **Faucet Dontaion Address:** `' + config.faucet.faucet_wallet_pub + '`\n**Tipbot Donation Address:** `' + config.bot_details.bot_donationAddress + '`');
 
   },
 };
