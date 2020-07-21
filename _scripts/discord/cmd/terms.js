bbe39dac5c43fb10e67fdea5600da3a09240deae
fr1t2 <fr1t2@protonmail.com>
2020-07-21 16:18:42 -0600

    Update terms

--- _scripts/discord/cmd/terms.js
+++ _scripts/discord/cmd/terms.js
@@ -8,7 +8,8 @@ module.exports = {
   cooldown: 0,
   execute(message) {
     message.reply(' Check your DM\'s')
-    message.author.send(` 
+    message.author.send(`
+
             __**TipBot Terms and Conditions**__
 
 :small_orange_diamond: Use of this TipBot and any function it may provide to you, as the user, is at your risk.
@@ -39,25 +40,25 @@ __**You assume all risk by using this service**__
                     message.author.send(`
            :exclamation: __**RULES**__ :exclamation:
 
-:exclamation: *All tips are final once sent.* 
+:diamond_shape_with_a_dot_inside:  *All tips are final once sent.* 
 
-:exclamation: *Tips will never be refunded or returned to a user, for any reason.*
+:diamond_shape_with_a_dot_inside: *Tips will never be refunded or returned to a user, for any reason.*
 
-:exclamation: *This service is for tipping or giving small amounts of QRL to other users.*
+:diamond_shape_with_a_dot_inside: *This service is for tipping or giving small amounts of QRL to other users.*
 
-:exclamation: *You agree to not store or trade currency or for any other reason than tipping users.*
+:diamond_shape_with_a_dot_inside: *You agree to not store or trade currency or for any other reason than tipping users.*
 
-:exclamation: *You will not store large amounts of QRL in this address at any time.*
+:diamond_shape_with_a_dot_inside: *You will not store large amounts of QRL in this address at any time.*
 
-:exclamation: *You take full responsibility for transferring funds out of the Tipbot, using the \`+transfer\` function into wallets you control.*
+:diamond_shape_with_a_dot_inside: *You take full responsibility for transferring funds out of the Tipbot, using the \`+transfer\` function into wallets you control.*
 
-:exclamation: *You will not use this bot if it will in any way break any law, in any jurisdiction. \`+opt-out\` to disable your account.*
+:diamond_shape_with_a_dot_inside: *You will not use this bot if it will in any way break any law, in any jurisdiction. \`+opt-out\` to disable your account.*
 
-:exclamation: *You will not use this bot in any way that is not intended or identified in these rules.*
+:diamond_shape_with_a_dot_inside: *You will not use this bot in any way that is not intended or identified in these rules.*
 
-:exclamation: *Any tips sent to a user that has not signed up will be saved by the bot for that user. Failure of the user to collect tips in this time may result in a loss of funds for that user and they will not be returned to the sender. These lost tips will fund the faucet and cover any operational costs that may incur.*
+:diamond_shape_with_a_dot_inside: *Any tips sent to a user that has not signed up will be saved by the bot for that user. Failure of the user to collect tips in this time may result in a loss of funds for that user and they will not be returned to the sender. These lost tips will fund the faucet and cover any operational costs that may incur.*
 
-:exclamation: *Any abuse of the service will result in a ban, and if warranted legal action may be taken accordingly. Funds will not be returned to banned users.*
+:diamond_shape_with_a_dot_inside: *Any abuse of the service will result in a ban, and if warranted legal action may be taken accordingly. Funds will not be returned to banned users.*
 
 **You must \`+agree\` with these terms to use the bot!**
                     `);
