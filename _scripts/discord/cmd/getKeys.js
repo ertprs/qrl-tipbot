7ea00e8326e16bbd51534d7fa77506e6ef1672e1
fr1t2 <fr1t2@protonmail.com>
2020-07-22 08:20:24 -0600

    user response

--- _scripts/discord/cmd/getKeys.js
+++ _scripts/discord/cmd/getKeys.js
@@ -63,7 +63,7 @@ module.exports = {
           const userSecretKeyPromise = secretKey(walletPub);
           userSecretKeyPromise.then(function(userSecrets) {
             const keys = JSON.stringify(JSON.parse(userSecrets));
-            console.log(keys)
+            console.log(JSON.stringify(userSecrets))
             const embed = new Discord.MessageEmbed()
               .setColor(0x000000)
               .setTitle('**TipBot Secret Info**')
