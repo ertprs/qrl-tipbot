e143d99a423596c05e88263ef9140a3bf60f7e1f
fr1t2 <fr1t2@protonmail.com>
2020-07-24 00:01:04 -0600

    info testing

--- _scripts/discord/cmd/info.js
+++ _scripts/discord/cmd/info.js
@@ -150,9 +150,9 @@ module.exports = {
 
           { name: 'Market Cap:', value: '`\u0024 ' + usdMarketCap + '`', inline: true },
           { name: 'Volume', value: '`\u0024 ' + usdTotalVolume + '`', inline: true },
+          { name: 'Circulating Supply', value: '`' + circulatingSupply + ' / ' + totalSupply + '`' },
           { name: '24hr Low ', value: '`\u0024 ' + usdLow24h + '`', inline: true },
           { name: '24hr High', value: '`\u0024 ' + usdHigh24h + '`', inline: true },
-          { name: 'Circulating Supply', value: '`' + circulatingSupply + ' / ' + totalSupply + '`' },
         )
         .setTimestamp()
         .setFooter('Market data provided by Coin Gecko, ');
