a2e41e6e1d82a2e34ec1a64c2eaf546c7bd5a258
fr1t2 <fr1t2@protonmail.com>
2020-07-27 09:55:53 -0600

    output cleanup

--- _scripts/discord/cmd/getKeys.js
+++ _scripts/discord/cmd/getKeys.js
@@ -69,10 +69,11 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
               .setColor('RED')
               .setTitle('**TipBot Secret Info**')
-              .setDescription('**__Protect These Keys!!__** They give anyone access to all of the funds in your tipbot address!\nUse the __**[QRL Web Wallet](' + config.wallet.wallet_url + ')**__ to withdraw funds from your Tipbot account')
+              .setDescription('**__Protect These Keys!!__** They give anyone access to all of the funds in your tipbot address!')
               .addField('Tipbot Secret Keys', '__WARNING:__ **NEVER SHARE THESE KEYS WITH ANYONE!!**')
               .addField('Hexseed: ', '||' + keys.hexseed + '||')
               .addField('Mnemonic: ', '||' + keys.mnemonic + '||')
+              .addField('Use the QRL Web Wallet to withdraw funds from your Tipbot account', '__**[QRL Web Wallet Link](' + config.wallet.wallet_url + ')**__')
               .setFooter('.: The QRL Contributors :.');
             message.author.send({ embed })
               .then(() => {
