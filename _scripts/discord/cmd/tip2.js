51df961f73b873025c60bd6bbc68a866c37804d7
fr1t2 <fr1t2@protonmail.com>
2020-07-28 22:05:23 -0600

    tip cleanup v2

--- _scripts/discord/cmd/tip2.js
+++ _scripts/discord/cmd/tip2.js
@@ -128,7 +128,7 @@ module.exports = {
     console.log('message.mentions.users.size' + message.mentions.users.size);
     // console.log('args' + args);
     checks(tip);
-    console.log('tipAmount: ' + tip.toString());
+    console.log('tipAmount: ' + tip..toString());
 
 
 // We have users mentioned, get the tipList into map
