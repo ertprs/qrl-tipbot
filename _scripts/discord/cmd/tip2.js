5f7d2ffa1ac023665f18aa368dc5914c8dda5f9d
fr1t2 <fr1t2@protonmail.com>
2020-07-28 22:19:11 -0600

    tip cleanup v2

--- _scripts/discord/cmd/tip2.js
+++ _scripts/discord/cmd/tip2.js
@@ -85,11 +85,12 @@ module.exports = {
         ReplyMessage('Please enter a valid amount to tip! +tip {AMOUNT} @USER(\'s)');
         return ;
       }
+      const fee = toShor(config.wallet.tx_fee)
       // Check that tip amount is above fee
-      if (amount < toShor(config.wallet.tx_fee)) {
+      if (amount < fee) {
         message.channel.stopTyping(true);
         console.log('tipAmount < config.wallet.tx_fee');
-        ReplyMessage('Please enter a valid amount to tip! Must be more than the fee `{' + toShor(config.wallet.tx_fee) + '}` +tip {AMOUNT} @USER(\'s)');
+        ReplyMessage('Please enter a valid amount to tip! Must be more than the fee `{' + config.wallet.tx_fee + '}` +tip {AMOUNT} @USER(\'s)');
         return ;
       }
 
