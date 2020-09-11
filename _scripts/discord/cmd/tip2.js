f87e38495f36b92ab39a939bc251c209eafb7758
fr1t2 <fr1t2@protonmail.com>
2020-09-11 12:15:34 -0600

    Progress

--- _scripts/discord/cmd/tip2.js
+++ _scripts/discord/cmd/tip2.js
@@ -136,7 +136,7 @@ module.exports = {
       // Check if mentions user
       console.log('message.mentions.users' + JSON.stringify(message.mentions));
       
-      if (message.mentions.users.includes(message.author)) {
+      if (message.mentions.includes(users.message.author)) {
         console.log('can\'t tip yourself, message.mentions.users.first() == message.author');
         ReplyMessage('You can\'t tip yourself');
         message.channel.stopTyping(true);
