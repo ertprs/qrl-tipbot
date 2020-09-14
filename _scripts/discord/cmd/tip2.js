5507dcaa15901ff6beb5c1ca81f529687d5a63ec
fr1t2 <fr1t2@protonmail.com>
2020-09-13 22:57:49 -0600

    Progress

--- _scripts/discord/cmd/tip2.js
+++ _scripts/discord/cmd/tip2.js
@@ -157,7 +157,7 @@ module.exports = {
         const oodate = tippingUserOptOut_Date.toString();
 
         console.log('Datetime ' + oodate.toLocaleString(DateTime.DATETIME_MED));
-        console.log('Datetime fromISO ' + DateTime.fromISO(oodate);
+        console.log('Datetime fromISO ' + DateTime.fromISO(oodate));
 
         ReplyMessage('User opt\'ed out of the bot on ' + DateTime.fromISO(toString(tippingUserOptOut_Date)) + '. Please opt back in to use the bot. `+opt-in`');
         return;
