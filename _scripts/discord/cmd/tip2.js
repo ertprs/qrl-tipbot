7adcc6ca81d6b3cd8b4f87f45f32a9f0639007e7
fr1t2 <fr1t2@protonmail.com>
2020-09-26 16:59:08 -0600

    More Progress

--- _scripts/discord/cmd/tip2.js
+++ _scripts/discord/cmd/tip2.js
@@ -92,7 +92,7 @@ module.exports = {
       // send the users data to future_tips for when they signup
       return new Promise(resolve => {
         console.log('tipToDBWrite tipToInfo' + JSON.stringify(tipToInfo));
-        const addToTipsToDBinfo = { from_user_id: userID, user_id: tipToInfo.user_id, tip_amt: tipToInfo.tip_amount, future_tip_id: tipToInfo.future_tip_id, time_stamp: new Date() };
+        const addToTipsToDBinfo = { from_user_id: userID, user_id: tipToInfo.user_id, tip_amt: tipToInfo.tip_amt, future_tip_id: tipToInfo.future_tip_id, time_stamp: new Date() };
         console.log('tipToDBWrite addToTipsToDBinfo: ' + JSON.stringify(addToTipsToDBinfo));
         const addToTipsToDBinfoWrite = dbHelper.addTipTo(addToTipsToDBinfo);
         resolve(addToTipsToDBinfoWrite);
