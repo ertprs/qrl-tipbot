7346af540858d87cc8e9ee434b621c99e64677ee
fr1t2 <fr1t2@protonmail.com>
2020-09-29 12:38:11 -0600

    More Progress

--- _scripts/discord/cmd/tip2.js
+++ _scripts/discord/cmd/tip2.js
@@ -46,7 +46,7 @@ module.exports = {
       }, 1000);
     }
 
-    function errorMessage(content, footer = '[.: Tipbot provided by The QRL Contributors :.](https://github.com/theqrl-community)') {
+    function errorMessage(content, footer = '\t.: Tipbot provided by The QRL Contributors :.') {
       setTimeout(function() {
         const embed = new Discord.MessageEmbed()
           .setColor(0x000000)
