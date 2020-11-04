11bb68dad365909500f5ea79758c4d2edb3b5ac5
fr1t2 <fr1t2@protonmail.com>
2020-11-03 21:47:07 -0700

    update

--- _scripts/discord/cmd/tip2.js
+++ _scripts/discord/cmd/tip2.js
@@ -497,11 +497,11 @@ module.exports = {
             }
             message.channel.stopTyping(true);
             if (tipUserCount > 1) {
-              replyMessage({ header: 'Tip Sent!', description: 'you tipped ' + tippedUserIDs + ',' + futureTippedUserIDs + ' `' + toQuanta(givenTip) + '` QRL each.', content: '*All tips are on-chain, and will take some time to process.*' });
+              replyMessage({ header: "'Tip Sent!'", description: "'you tipped ' + tippedUserIDs + ',' + futureTippedUserIDs + ' `' + toQuanta(givenTip) + '` QRL each.'", content: '*All tips are on-chain, and will take some time to process.*' });
               // ReplyMessage('you tipped ' + tippedUserIDs + ',' + futureTippedUserIDs + ' `' + toQuanta(givenTip) + '` QRL each.\n*All tips are on-chain, and will take some time to process.*');
             }
             else {
-              replyMessage({ header: 'Tip Sent!', description: 'you tipped ' + tippedUserIDs + ',' + futureTippedUserIDs + ' `' + toQuanta(givenTip) + '` QRL.', content: '*All tips are on-chain, and will take some time to process.*' });
+              replyMessage({ header: "'Tip Sent!'", description: "'you tipped ' + tippedUserIDs + ',' + futureTippedUserIDs + ' `' + toQuanta(givenTip) + '` QRL.'", content: '*All tips are on-chain, and will take some time to process.*' });
               // ReplyMessage('you tipped ' + tippedUserIDs + ',' + futureTippedUserIDs + ' `' + toQuanta(givenTip) + '` QRL.\n*All tips are on-chain, and will take some time to process.*');
             }
           //  console.log('futureTippedUserIDs: ' + JSON.stringify(futureTippedUserIDs));
