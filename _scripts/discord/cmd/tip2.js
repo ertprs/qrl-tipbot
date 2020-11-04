246f0e12c03f0c096c031225320a5663460c297c
fr1t2 <fr1t2@protonmail.com>
2020-11-03 22:00:51 -0700

    update

--- _scripts/discord/cmd/tip2.js
+++ _scripts/discord/cmd/tip2.js
@@ -467,7 +467,7 @@ module.exports = {
               .addField('You Tipped', tippedUserIDs + ' ' + futureTippedUserIDs)
               .addField('Total Transfer', '**' + toQuanta(tipTotal).toFixed(9) + ' QRL**', true)
               .addField('New Wallet Balance', '[**' + newWal_bal + ' QRL**](' + config.bot_details.explorer_url + '/a/' + tippingUserWalPub + ')', true)
-              .addField('Transaction Hash', '```css' + tx_hash + '```')
+              .addField('Transaction Hash', '```css\n' + tx_hash + '\n```')
               .setFooter('.: The QRL Contributors :.');
 
             message.author.send({ embed })
