bb2b60383fc0e616e7e60cbfc4e0e26fd11557dd
fr1t2 <fr1t2@protonmail.com>
2020-11-21 22:41:58 -0700

    Agree updated and functional fixing bug requiring multiple user input to establish signup

--- _scripts/discord/cmd/agree.js
+++ _scripts/discord/cmd/agree.js
@@ -48,7 +48,6 @@ module.exports = {
       });
     }
 
-
     // add user to agree db. Function expects { service: , user_id: } as usrAgree
     async function agreeDBWrite(botUserId) {
       return new Promise(resolve => {
@@ -61,20 +60,19 @@ module.exports = {
       });
     }
 
-
     // check if user exists
-
     async function main() {
       const userInfo = await getUserInfo({ service: 'discord', service_id: userID });
       console.log('userInfo: ' + JSON.stringify(userInfo));
       return userInfo;
     }
-
+    // Add agree flag to users_agree database
     async function userAgreeAdd(info) {
       const agreeAdd = await agreeDBWrite(info);
       return agreeAdd;
     }
 
+    // check for user then if found check for already agreed, if not then set
     main().then(function(infoReturned) {
       const userFound = infoReturned[0].user_found;
       if (userFound) {
@@ -89,87 +87,15 @@ module.exports = {
           const botUserId = infoReturned[0].user_id;
           userAgreeAdd(botUserId).then(function(agreeReturn) {
             console.log('agreeReturn: ' + JSON.stringify(agreeReturn));
-            ReplyMessage('You\'ve agreed to the bots terms, thanks! **You can now use the tipbot**');
+            ReplyMessage('You\'ve agreed :white_check_mark: You can now use the tipbot!');
           });
         }
       }
       else {
         console.log('userNotFound');
         errorMessage({ error: 'No Account Found!', description: 'You must signup to use the tipbot. enter `+help` for bot instructions' });
-
       }
     });
 
-    // delete the message after user sends agree request if not in a DM
-    if(message.guild != null) {
-      message.delete();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-    const dbHelper = require('../../db/dbHelper');
-    const uuid = `${message.author}`;
-    const UUID = uuid.slice(1, -1);
-    const GetAllUserInfo = dbHelper.GetAllUserInfo;
-    const info = JSON.parse(JSON.stringify({ service: 'discord', service_id: UUID }));
-    const found = GetAllUserInfo(info);
-
-    // check for the message author. If not found fail
-    found.then(function(foundRes) {
-      // console.log('foundRes ' + JSON.stringify(foundRes));
-      // check for user found
-      const user_found = foundRes[0].user_found;
-      if (user_found !== 'true') {
-        // if the user is not found...
-        message.channel.startTyping();
-        setTimeout(function() {
-          message.channel.stopTyping(true);
-          message.reply('\nPlease sign up first, `+add`');
-        }, 1000);
-        return;
-      }
-      else {
-        // user found, check for agree
-        // set the user ID
-        const user_id = foundRes[0].user_id;
-        // console.log('user_id: ' + user_id);
-      // user found, check for already set agree
-        const check_info = { service: 'discord', user_id: user_id };
-        // console.log('check_info: ' + JSON.stringify(check_info));
-        if (foundRes[0].user_agree == 'false') {
-          // user has not agreed...
-          const agree = dbHelper.agree({ service: 'discord', user_id: user_id });
-          agree.then(function(results) {
-            // message user of status
-            message.channel.startTyping();
-            setTimeout(function() {
-              message.author.send('Thanks for giving your consent to use this service!');
-              message.channel.stopTyping(true);
-            }, 500);
-            return results;
-          });
-        }
-        else {
-          message.author.send('You have already agreed!');
-          return;
-        }
-       return;
-      }
-    });
-    */
   },
 };
