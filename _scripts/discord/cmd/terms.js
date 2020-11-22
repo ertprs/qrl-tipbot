f44318fc69f13ef15282b276c68b5dad68656f46
fr1t2 <fr1t2@protonmail.com>
2020-11-21 23:32:03 -0700

    updates and formatting

--- _scripts/discord/cmd/terms.js
+++ _scripts/discord/cmd/terms.js
@@ -16,7 +16,6 @@ __**TipBot Terms and Conditions**__
 :small_orange_diamond: Abuse of this service may result in a ban from the service and if warranted legal action may be taken. 
 :small_orange_diamond: By using this service you agree to share information about your social media account used for signup to the TipBot service including but not limited to, service user name(s), service user ID(s), all interactions and messages with the bot, and any other public information available through the social media API services.
 :small_orange_diamond: At no point will this information be sold or used for any purpose other than this TipBot service, and is only stored for the purpose of managing your accounts.
-:small_orange_diamond: The Tip Bot will never share any Private Keys with the user. 
 :small_orange_diamond: All funds must be withdrawn to a user controlled account. 
 :small_orange_diamond: Any funds left on the bot may be lost at any time, and the user agrees that this is an acceptable loss. 
 :small_orange_diamond: Funds shall be withdrawn from the bot regularly into user controlled wallets.
@@ -26,15 +25,15 @@ __**You assume all risk by using this service**__
                     `);
     message.author.send(`
 :exclamation: __**RULES**__ :exclamation:
-:diamond_shape_with_a_dot_inside:  *All tips are final once sent.* 
+:diamond_shape_with_a_dot_inside: *All tips are final once sent.* 
 :diamond_shape_with_a_dot_inside: *Tips will never be refunded or returned to a user, for any reason.*
 :diamond_shape_with_a_dot_inside: *This service is for tipping or giving small amounts of QRL to other users.*
 :diamond_shape_with_a_dot_inside: *You agree to not store or trade currency or for any other reason than tipping users.*
 :diamond_shape_with_a_dot_inside: *You will not store large amounts of QRL in this address at any time.*
-:diamond_shape_with_a_dot_inside: *You take full responsibility for transferring funds out of the Tipbot, using the \`+transfer\` function into wallets you control.*
+:diamond_shape_with_a_dot_inside: *You take full responsibility for transferring funds out of the Tipbot, using the \`+transfer\` function into a wallet you control.*
 :diamond_shape_with_a_dot_inside: *You will not use this bot if it will in any way break any law, in any jurisdiction. \`+opt-out\` to disable your account.*
 :diamond_shape_with_a_dot_inside: *You will not use this bot in any way that is not intended or identified in these rules.*
-:diamond_shape_with_a_dot_inside: *Any tips sent to a user that has not signed up will be saved by the bot for that user. Failure of the user to collect tips in this time may result in a loss of funds for that user and they will not be returned to the sender. These lost tips will fund the faucet and cover any operational costs that may incur.*
+:diamond_shape_with_a_dot_inside: *Any tips sent to a user that has not signed up will be saved by the bot for that user. Failure of the user to collect tips may result in a loss of funds for that user. They will not be returned to the sender.*
 :diamond_shape_with_a_dot_inside: *Any abuse of the service will result in a ban, and if warranted legal action may be taken accordingly. Funds will not be returned to banned users.*
 
 **You must \`+agree\` with these terms to use the bot!**
