425a981507ae1cfa6955fb1ce0f9254d49b5636c
fr1t2 <fr1t2@protonmail.com>
2020-11-21 21:14:15 -0700

    Rework and fixes plus new script

--- _scripts/discord/cmd/agree.js
+++ _scripts/discord/cmd/agree.js
@@ -7,6 +7,97 @@ module.exports = {
   usage: '\n## **agree** - Agree to the terms and conditions of the tipbot.  ',
   // execute(message, args) {
   execute(message) {
+    // const Discord = require('discord.js');
+    const dbHelper = require('../../db/dbHelper');
+    // const config = require('../../../_config/config.json');
+    const username = `${message.author}`;
+    const userID = username.slice(1, -1);
+
+    // start the bot typing
+    message.channel.startTyping();
+
+/*
+    // default reply message format
+    // ReplyMessage('**You\'ve sent a `' + toQuanta(givenTip) + ' QRL` tip to ' + tippedUserIDs + ',' + futureTippedUserIDs + ' each**. Thanks for using the tipbot!\n*All tips are on-chain, and will take some time to process.*');
+    function ReplyMessage(content) {
+      setTimeout(function() {
+        message.reply(content);
+        message.channel.stopTyping(true);
+      }, 1000);
+    }
+
+    // default error message format
+    // errorMessage({ error: 'No User(s) Mentioned...', description: 'Who are you tipping? enter `+help tip` for instructions' });
+    function errorMessage(content, footer = '  .: Tipbot provided by The QRL Contributors :.') {
+      setTimeout(function() {
+        const embed = new Discord.MessageEmbed()
+          .setColor(0x000000)
+          .setTitle(':warning:  ERROR: ' + content.error)
+          .setDescription(content.description)
+          .setFooter(footer);
+        message.reply({ embed });
+        message.channel.stopTyping(true);
+      }, 1000);
+    }
+*/
+    // Get user info. Function expects { service: service, service_id: service_id } as usrInfo
+    async function getUserInfo(usrInfo) {
+      return new Promise(resolve => {
+        console.log('getUserInfo(usrInfo) ' + JSON.stringify(usrInfo));
+        const data = dbHelper.GetAllUserInfo(usrInfo);
+        resolve(data);
+      });
+    }
+
+/*
+    async function tipbotInfo(ID) {
+      // FIX ME HERE!!!
+      return new Promise(resolve => {
+        const userInfo = getUserInfo({ service: 'discord', service_id: ID });
+        resolve(userInfo);
+      });
+    }
+*/
+
+/*
+    // add user to agree db. Function expects { service: , user_id: } as usrAgree
+    async function agreeDBWrite(usrAgree) {
+      return new Promise(resolve => {
+        console.log('agreeDBWrite(usrInfo) ' + JSON.stringify(tipInfo));
+        const addToTipsDBinfo = { from_user_id: tipInfo.from_user_id, tip_amount: toQuanta(tipInfo.tip_amount), from_service: 'discord', time_stamp: new Date() };
+        //  console.log('tipDBWrite addToTipsDBinfo: ' + JSON.stringify(addToTipsDBinfo));
+        const addToTipsDBinfoWrite = dbHelper.addTip(addToTipsDBinfo);
+        resolve(addToTipsDBinfoWrite);
+      });
+    }
+
+*/
+    // check if user exists
+
+    const userInfo = getUserInfo(userID);
+    console.log('userInfo: ' + userInfo);
+
+    // delete the message after user sends agree request if not in a DM
+    if(message.guild != null) {
+      message.delete();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
     const dbHelper = require('../../db/dbHelper');
     const uuid = `${message.author}`;
     const UUID = uuid.slice(1, -1);
@@ -14,9 +105,6 @@ module.exports = {
     const info = JSON.parse(JSON.stringify({ service: 'discord', service_id: UUID }));
     const found = GetAllUserInfo(info);
 
-    if(message.guild != null) {
-      message.delete();
-    }
     // check for the message author. If not found fail
     found.then(function(foundRes) {
       // console.log('foundRes ' + JSON.stringify(foundRes));
@@ -59,5 +147,6 @@ module.exports = {
        return;
       }
     });
+    */
   },
 };
