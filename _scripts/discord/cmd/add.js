55afe7eda8fccb287af1e1246fc4e6881e94de3d
fr1t2 <fr1t2@protonmail.com>
2020-12-11 22:28:38 -0700

    Reply to error for DM block

--- _scripts/discord/cmd/add.js
+++ _scripts/discord/cmd/add.js
@@ -26,7 +26,7 @@ module.exports = {
     const checkUserpromise = checkUser(user_info);
     const getBalance = wallet.GetBalance;
 
-        // use to send a reply to user with delay and stop typing
+    // use to send a reply to user with delay and stop typing
     // ReplyMessage(' Check your DM\'s');
     function ReplyMessage(content) {
       message.channel.startTyping();
@@ -111,18 +111,19 @@ module.exports = {
               message.author.send({ embed })
                 .then(() => {
                   if (message.channel.type === 'dm') return;
-                  message.channel.stopTyping(true);
-                  ReplyMessage('\nYou\'re signed up already. :thumbsup:\nTry `+help`');
+                  // message.channel.stopTyping(true);
+                  errorMessage({ error: 'User Found In System...', description: 'You\'re signed up already. :thumbsup:\nTry `+help`' });
+                  // ReplyMessage('\nYou\'re signed up already. :thumbsup:\nTry `+help`');
                 })
                 .catch(error => {
                   errorMessage({ error: 'Direct Message Disabled', description: 'It seems you have DM\'s blocked, please enable and try again...' });
-                  //console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                  //message.channel.stopTyping(true);
-                  //message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+                  // console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                  // message.channel.stopTyping(true);
+                  // message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
                 });
             });
           });
-          message.channel.stopTyping(true);
+          // message.channel.stopTyping(true);
           return output;
         }
         else if (found === 'false') {
@@ -144,7 +145,7 @@ module.exports = {
               }
               const userInfo = { service: 'discord', service_id: discord_id, user_name: MessageAuthorUsername, wallet_pub: wallet_pub, wallet_bal: 0, user_key: salt, user_auto_created: false, auto_create_date: new Date(), opt_out: false, optout_date: new Date(), drip_amt: dripamt };
               // console.log('userInfo:' + JSON.stringify(userInfo));
-              message.channel.stopTyping();
+              // message.channel.stopTyping();
               return userInfo;
             }).then(function(userInfo) {
             // add user to the database and create an account
@@ -192,62 +193,61 @@ module.exports = {
                   .addField('For all of my commands:\t', '`+help`');
                 message.author.send({ embed })
                   .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.channel.stopTyping(true);
-                    ReplyMessage('it seems like I can\'t DM you! Enable DM and try `+add` again...');
+                    // console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                    // message.channel.stopTyping(true);
+                    errorMessage({ error: 'Direct Message Disabled', description: 'It seems you have DM\'s blocked, please enable and try again...' });
+                    // ReplyMessage('it seems like I can\'t DM you! Enable DM and try `+add` again...');
                     // react to the users message for fun
                   }).then(() => {
                     if (message.channel.type === 'dm') return;
                     message.author.send(` 
-            __**TipBot Terms and Conditions**__
-
-Use of this TipBot and any function it may provide to you, as the user, is at your risk. By using this service you agree to not hold liable, for any reasons, the owner, operators, or any affiliates of the QRL TipBot, qrl.tips or any other party associated with this service.
-
+__**TipBot Terms and Conditions**__
+:small_orange_diamond: Use of this TipBot and any function it may provide to you, as the user, is at your risk.
+:small_orange_diamond: By using this service you agree to not hold liable, for any reasons, the owner, operators, or any affiliates of the QRL TipBot or anyone associated with this service.
 :small_orange_diamond: By using this service, you agree to not abuse or misuse the service and will follow the rules listed below. 
 :small_orange_diamond: Abuse of this service may result in a ban from the service and if warranted legal action may be taken. 
 :small_orange_diamond: By using this service you agree to share information about your social media account used for signup to the TipBot service including but not limited to, service user name(s), service user ID(s), all interactions and messages with the bot, and any other public information available through the social media API services.
 :small_orange_diamond: At no point will this information be sold or used for any purpose other than this TipBot service, and is only stored for the purpose of managing your accounts.
-:small_orange_diamond: The Tip Bot will never share any Private Keys with the user. 
 :small_orange_diamond: All funds must be withdrawn to a user controlled account. 
 :small_orange_diamond: Any funds left on the bot may be lost at any time, and the user agrees that this is an acceptable loss. 
 :small_orange_diamond: Funds shall be withdrawn from the bot regularly into user controlled wallets.
 :small_orange_diamond: Users will not store large amounts of funds in any tipbot wallet
 
-**You assume all risk by using this service**
+__**You assume all risk by using this service**__
 
                     `)
                       .catch(error => {
+                        errorMessage({ error: 'Direct Message Disabled', description: 'It seems you have DM\'s blocked, please enable and try again...' });
                         // console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                        ReplyMessage('It seems like I can\'t DM you! Enable DM and try again...');
+                        // ReplyMessage('It seems like I can\'t DM you! Enable DM and try again...');
                         // deleteMessage();
-                      });
-                    message.author.send(`
-           :exclamation: __**RULES**__ :exclamation:
+                      }).then(function() {
 
-:exclamation: *All tips are final once sent. Tips will never be refunded or returned to a user, for any reason.*
-:exclamation: *This service is for tipping or giving small amounts of QRL to other users.*
-:exclamation: *You agree to not store or trade currency or for any other reason than tipping users.*
-:exclamation: *You will not store large amounts of QRL in this address at any time.*
-:exclamation: *You take full responsibility for transferring funds out of the Tipbot, using the \`+transfer\` function into wallets you control.*
-:exclamation: *You will not use this bot if it will in any way break any law, in any jurisdiction. \`+opt-out\` to disable your account.*
-:exclamation: *You will not use this bot in any way that is not intended or identified in these rules.*
-:exclamation: *Any tips sent to a user that has not signed up will be saved by the bot for that user. Failure of the user to collect tips in this time may result in a loss of funds for that user and they will not be returned to the sender. These lost tips will fund the faucet and cover any operational costs that may incur.*
-:exclamation: *Any abuse of the service will result in a ban, and if warranted legal action may be taken accordingly. Funds will not be returned to banned users.*
+                        message.author.send(`
+:exclamation: __**RULES**__ :exclamation:
+:diamond_shape_with_a_dot_inside: *All tips are final once sent.* 
+:diamond_shape_with_a_dot_inside: *Tips will never be refunded or returned to a user, for any reason.*
+:diamond_shape_with_a_dot_inside: *This service is for tipping or giving small amounts of QRL to other users.*
+:diamond_shape_with_a_dot_inside: *You agree to not store or trade currency or for any other reason than tipping users.*
+:diamond_shape_with_a_dot_inside: *You will not store large amounts of QRL in this address at any time.*
+:diamond_shape_with_a_dot_inside: *You take full responsibility for transferring funds out of the Tipbot, using the \`+transfer\` function into a wallet you control.*
+:diamond_shape_with_a_dot_inside: *You will not use this bot if it will in any way break any law, in any jurisdiction. \`+opt-out\` to disable your account.*
+:diamond_shape_with_a_dot_inside: *You will not use this bot in any way that is not intended or identified in these rules.*
+:diamond_shape_with_a_dot_inside: *Any tips sent to a user that has not signed up will be saved by the bot for that user. Failure of the user to collect tips may result in a loss of funds for that user. They will not be returned to the sender.*
+:diamond_shape_with_a_dot_inside: *Any abuse of the service will result in a ban, and if warranted legal action may be taken accordingly. Funds will not be returned to banned users.*
 
-If you **AGREE** to these terms, please type: \`+agree\`
-If you **DO NOT AGREE** to these terms, please type: \`+opt-out\`
-                    `)
+**You must \`+agree\` with these terms to use the bot!**
+                    `);
+                      })
                       .catch(error => {
                         // console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                        ReplyMessage('It seems like I can\'t DM you! Enable DM and try again...');
+                        errorMessage({ error: 'Direct Message Disabled', description: 'It seems you have DM\'s blocked, please enable and try again...' });
                         // deleteMessage();
                       });
-                    message.channel.stopTyping(true);
                     ReplyMessage(':white_check_mark: Your signed up! Please `+agree` to my terms to begin using the bot. For a list of my commands type `+help`');
                   })
                   .catch(error => {
                     console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.channel.stopTyping(true);
                     ReplyMessage('it seems like I can\'t DM you! Enable DM and try `+add` again...');
                   // react to the users message for fun
                   });
@@ -255,7 +255,6 @@ If you **DO NOT AGREE** to these terms, please type: \`+opt-out\`
                   .then(() => message.react('🇷'))
                   .then(() => message.react('🇱'))
                   .catch(() => console.error('One of the emojis failed to react.'));
-                message.channel.stopTyping(true);
                 return userresponse;
               });
             });
