60b0e0dc6ad0a3ba05363ef7803fd8024ad607f6
fr1t2 <fr1t2@protonmail.com>
2020-12-13 09:14:46 -0700

    if not the tipbot add to bot list

--- _scripts/discord/cmd/tip.js
+++ _scripts/discord/cmd/tip.js
@@ -279,7 +279,7 @@ module.exports = {
         return el != null;
       });
 
-      console.log('filteredTipList: ' + JSON.stringify(filteredTipList));
+      // console.log('filteredTipList: ' + JSON.stringify(filteredTipList));
 
       // get the bots into array
       const botList = message.mentions.users.map(user => {
@@ -312,7 +312,7 @@ module.exports = {
         for(let i = 0, l = filteredBotList.length; i < l; i++) {
           bots.push(' ' + filteredBotList[i].userid);
         }
-        errorMessage({ error: 'Bots Mentioned In Tip...', description: 'You can\'t tip robots! Removing ' + bots + ' from the tip.' });
+        // errorMessage({ error: 'Bots Mentioned In Tip...', description: 'You can\'t tip robots! Removing ' + bots + ' from the tip.' });
         // ReplyMessage('No bot tipping allowed! Removing the robots and sending to the rest...\rBots mentioned ' + bots);
       }
       const tipListJSON = JSON.parse(JSON.stringify(filteredTipList));
