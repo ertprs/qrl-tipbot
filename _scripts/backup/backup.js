0d4e10ecbd7be0ccbf5c248c68d77f672963cc16
fr1t2 <fr1t2@protonmail.com>
2020-12-13 16:05:50 -0700

    backup work

--- _scripts/backup/backup.js
+++ _scripts/backup/backup.js
@@ -15,6 +15,7 @@ const { Transform } = require('stream');
 const mysqldump = require('mysqldump');
 const date1 = Date.now();
 const sha256Array = [];
+const folderName = 'backup/';
 
 // Cryptographic functions for encryption
 function getCipherKey(password) {
@@ -22,15 +23,20 @@ function getCipherKey(password) {
 }
 
 
-function tarFiles(files) {
+async function tarFiles(files) {
   return new Promise(function(resolve) {
     tar.c(
-      { file: config.backup.location + 'fullBackup-' + date1 + '.tar.gz' },
+      { file: config.backup.location + 'full_tipbot_backup.tar.gz' },
       [files],
     ).then(_ => {
-      console.log('tar created');
+      // sha256sum the tar file now
+      const sha256Tar = sha256sum(config.backup.location + 'full_tipbot_backup.tar.gz');
+      fs.writeFile(config.backup.location + 'full_tipbot_backup_sha256sum.txt', JSON.stringify(sha256Tar), function(err) {
+        if (err) throw err;
+        console.log('full_tipbot_backup_sha256sum.txt! ');
+      });
+      console.log('tar created' + JSON.stringify(sha256Tar));
     });
-    // pipe(fs.createWriteStream(config.backup.location + 'fullBackup-' + date1 + '.tar.gz'))
     resolve();
   });
 }
@@ -124,7 +130,7 @@ async function sqlBackup() {
   return new Promise(function(resolve) {
     // dump the result straight to a compressed file
     const fileName = date1 + '_tipBotDatabase_Backup.sql';
-    const dumpFilePath = config.backup.location + fileName;
+    const dumpFilePath = config.backup.location + folderName + fileName;
     mysqldump({
       connection: {
         host: config.database.db_host,
@@ -142,100 +148,67 @@ async function sqlBackup() {
 }
 
 async function main() {
+  // check for and make if not exist backup dir
+  try {
+    if (!fs.existsSync(config.backup.location + folderName)) {
+      fs.mkdirSync(config.backup.location + folderName);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
   // get the sql database into a dump file.
   const sqlDumpFile = await sqlBackup();
   console.log('sqlDumpFile: ' + JSON.stringify(sqlDumpFile));
+
   const SqlDumpFile = sqlDumpFile[0];
   console.log('SqlDumpFile: ' + JSON.stringify(SqlDumpFile));
 
-  // tar the files into the backup location
-  const tarFileArray = [SqlDumpFile, config.backup.walletFile, config.backup.walletdLog, config.backup.nodeConfig, config.backup.faucetLog, config.backup.botConfigFile];
-  console.log('tarFileArray: ' + JSON.stringify(tarFileArray));
-
-  const tarMe = await tarFiles(tarFileArray);
-console.log('tarMe: ' + tarMe)
-  // sha256 the files and dump into file
-  for (let i = 0; i < tarFileArray.length; i++) {
-    let file = tarFileArray[i];
-    file = file.split('/').pop();
-    // console.log(file);
-    const sha256value = await sha256sum(tarFileArray[i]);
-    sha256Array.push({
-      key: file,
-      value: sha256value,
-    });
-  }
-  // write the sha256 info to file
-  fs.writeFile(config.backup.location + 'sha256sum.txt', JSON.stringify(sha256Array), function(err) {
-    if (err) return console.log(err);
-  // console.log('sha256sum file written');
-  });
-  const sha256Tar = await sha256sum(config.backup.location + 'fullBackup-' + date1 + '.tar.gz');
-  fs.appendFile(config.backup.location + 'sha256sum.txt', JSON.stringify(sha256Tar), function(err) {
+  fs.copyFile(config.backup.walletFile, config.backup.location + folderName + 'walletd.json', (err) => {
     if (err) throw err;
-    console.log('sha256Tar Saved! ' + JSON.stringify(sha256Tar));
+  // console.log('walletd.json was copied to ' + config.backup.location + folderName + 'walletd.json');
   });
-
-
-  /*
-
-  // copy the remaining files over
-  fs.copyFile(config.backup.walletFile, config.backup.location + 'walletd.json', (err) => {
+  fs.copyFile(config.backup.walletdLog, config.backup.location + folderName + 'walletd.log', (err) => {
     if (err) throw err;
-  // console.log('walletd.json was copied to ' + config.backup.location + 'walletd.json');
+  // console.log('walletd.log was copied to ' + config.backup.location + folderName + 'walletd.log');
   });
-  fs.copyFile(config.backup.walletdLog, config.backup.location + 'walletd.log', (err) => {
-    if (err) throw err;
-  // console.log('walletd.log was copied to ' + config.backup.location + 'walletd.log');
-  });
-  fs.copyFile(config.backup.nodeConfig, config.backup.location + 'config.yml', (err) => {
+  fs.copyFile(config.backup.nodeConfig, config.backup.location + folderName + 'config.yml', (err) => {
     if (err) throw err;
   // console.log('nodeConfig was copied to ' + config.backup.location + 'config.yml');
   });
-  fs.copyFile(config.backup.faucetLog, config.backup.location + 'faucet.log', (err) => {
+  fs.copyFile(config.backup.faucetLog, config.backup.location + folderName + 'faucet.log', (err) => {
     if (err) throw err;
-  // console.log('faucetLog was copied to ' + config.backup.location + 'faucet.log');
+  // console.log('faucetLog was copied to ' + config.backup.location + folderName + 'faucet.log');
   });
-  // fs.copyFile(config.backup.botLogFile, config.backup.location + 'discord_bot.log', (err) => {
+  // fs.copyFile(config.backup.botLogFile, config.backup.location + folderName + 'discord_bot.log', (err) => {
   // if (err) throw err;
-  // console.log('botLogFile was copied to ' + config.backup.location + 'discord_bot.log');
+  // console.log('botLogFile was copied to ' + config.backup.location + folderName + 'discord_bot.log');
   // });
-  fs.copyFile(config.backup.botConfigFile, config.backup.location + 'config.json', (err) => {
+  fs.copyFile(config.backup.botConfigFile, config.backup.location + folderName + 'config.json', (err) => {
     if (err) throw err;
-  // console.log('botConfigFile was copied to ' + config.backup.location + 'config.json');
+  // console.log('botConfigFile was copied to ' + config.backup.location + folderName + 'config.json');
   });
 
-  // encrypt the sql DB backup??
-  encrypt({ file: sqlDumpFile[0], password: config.backup.encPass });
 
-  // define encrypted file name
-  const encSqlDumpFile = sqlDumpFile[0] + '.enc';
-  // console.log('file_encrypted');
-  // console.log(sqlDumpFile);
-
-  // const tarFileArray = [encSqlDumpFile, config.backup.walletFile, config.backup.walletdLog, config.backup.nodeConfig, config.backup.faucetLog, config.backup.botLogFile, config.backup.botConfigFile];
-
-  // tar the files into the backup location
-  const tarFileArray = [encSqlDumpFile, config.backup.walletFile, config.backup.walletdLog, config.backup.nodeConfig, config.backup.faucetLog, config.backup.botConfigFile];
-  // sha256sum those files
-  for (let i = 0; i < tarFileArray.length; i++) {
-    let file = tarFileArray[i];
-    file = file.split('/').pop();
-    // console.log(file);
-    const sha256value = await sha256sum(tarFileArray[i]);
+  // get the SHA256sum of each file
+  fs.readdirSync(config.backup.location + folderName).forEach(file => {
+    // get sha for each file in config.backup.botConfigFile, config.backup.location + folderName dir
+    const sha256value = sha256sum(file);
     sha256Array.push({
       key: file,
       value: sha256value,
     });
-  }
+    console.log(file);
+  });
+
   // write the sha256 info to file
-  fs.writeFile(config.backup.location + 'sha256sum.txt', JSON.stringify(sha256Array), function(err) {
+  fs.writeFile(config.backup.location + folderName + 'sha256sum.txt', JSON.stringify(sha256Array), function(err) {
     if (err) return console.log(err);
   // console.log('sha256sum file written');
   });
-*/
 
-
+  // tar the files in the the backup location
+  const tarMe = await tarFiles(config.backup.location + folderName);
   console.log('Backup Files written to ' + config.backup.location);
 }
 
