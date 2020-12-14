98713eb8b2f46fee03485472514362d3f431ade3
fr1t2 <fr1t2@protonmail.com>
2020-12-14 00:54:52 -0700

    write 0 to agree db

--- _scripts/db/dbHelper.js
+++ _scripts/db/dbHelper.js
@@ -44,6 +44,9 @@ async function GetAllUserInfo(args) {
         if (err) {
           console.log('[mysql error]', err);
         }
+        console.log('get_agree: ' + JSON.stringify(get_agree))
+        console.log('get_agree.length: ' + get_agree.length)
+        
         if(get_agree.length == 0) {
           // user has not agreed, not found in db users_agree but user is found
           foundResArray.push({ user_found: 'true', user_agree: 'false', opt_out: 'false', user_id: user_id });
@@ -64,7 +67,7 @@ async function GetAllUserInfo(args) {
         const opt_out = infoResult[0].opt_out;
         const optout_date = infoResult[0].optout_date;
         // const foundRes = { user_found: 'true' };
-        foundResArray.push({ user_found: 'true', user_agree: 'true', wallet_pub: wallet_pub, wallet_bal: wallet_bal, user_id: U_id, user_name: user_name, opt_out: opt_out, optout_date: optout_date });
+        foundResArray.push({ user_found: 'true', user_agree: 'false', wallet_pub: wallet_pub, wallet_bal: wallet_bal, user_id: U_id, user_name: user_name, opt_out: opt_out, optout_date: optout_date });
         // Array.prototype.push.apply(foundResArray, infoResult);
         // console.log('getAllInfoSearch foundResArray ' + JSON.stringify(foundResArray));
         resolve(foundResArray);
