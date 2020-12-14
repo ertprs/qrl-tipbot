96197ac6cc0295406ed5b77a58ac5ce3a00d6af6
fr1t2 <fr1t2@protonmail.com>
2020-12-13 17:17:05 -0700

    cleanup

--- _scripts/backup/backup.js
+++ _scripts/backup/backup.js
@@ -1,7 +1,5 @@
 // Script to backup all of the files needed to run the bot
-// curently dumps all files into the directory detailed in the config file config.backup.location
 // Run cron jobs to move these and tar things up plus send to another server.
-// Run this every hour, on the hour  0 */1 * * * /usr/bin/nodejs /home/$USER/qrl-tipbot/_scripts/backup/backup.js && /home/$USER/qrl-tipbot/_scripts/backup/backup.sh
 
 
 const fs = require('fs');
@@ -11,40 +9,6 @@ const mysqldump = require('mysqldump');
 const sha256Array = [];
 // const date1 = Date.now();
 const folderName = 'backup';
-
-/*
-function decryptFile({ fileName, password }) {
-  const input = fs.createReadStream(fileName + '.encrypted');
-  const output = fs.createWriteStream(fileName + '.unencrypted');
-  const initVect = crypto2.randomBytes(16);
-  const pass_hash = getCipherKey(password);
-  const CIPHER_KEY = new Buffer(pass_hash);
-  const decipher = crypto2.createDecipheriv('aes-256-cbc', CIPHER_KEY, initVect);
-  input.pipe(decipher).pipe(output).on('finish', () => {
-    console.log('FILE DECRYPTED');
-  }).on('error', error => {
-    console.log(error);
-  });
-}
-*/
-/*
-
-function encryptFile({ fileName, password }) {
-  const initVect = crypto2.randomBytes(16);
-  const pass_hash = getCipherKey(password);
-  const CIPHER_KEY = new Buffer(pass_hash);
-  const aes = crypto2.createCipheriv('aes-256-cbc', CIPHER_KEY, initVect);
-  const input = fs.createReadStream(fileName);
-  const output = fs.createWriteStream(fileName + '.encrypted');
-  input
-    .pipe(aes)
-    .pipe(output)
-    .on('finish', function() {
-      console.log('done encrypting');
-    });
-}
-
-*/
 
 // backup database into a and save in the backup folder
 async function sqlBackup() {
@@ -81,12 +45,7 @@ async function main() {
 
   // get the sql database into a dump file.
   const sqlDumpFile = await sqlBackup();
-  console.log('sqlDumpFile: ' + JSON.stringify(sqlDumpFile));
-
-  const SqlDumpFile = sqlDumpFile[0];
-  console.log('SqlDumpFile: ' + JSON.stringify(SqlDumpFile));
-
-
+  // console.log('sqlDumpFile: ' + JSON.stringify(sqlDumpFile));
   fs.copyFile(config.backup.walletFile, config.backup.location + folderName + '/walletd.json', (err) => {
     if (err) throw err;
   // console.log('walletd.json was copied to ' + config.backup.location + folderName + 'walletd.json');
@@ -112,42 +71,6 @@ async function main() {
   // console.log('botConfigFile was copied to ' + config.backup.location + folderName + 'config.json');
   });
 
-
-  /*
-  // get the SHA256sum of each file
-  let fileArray = [];
-  fileArray = [config.backup.location + folderName + '/tipBotDatabase_Backup.sql', config.backup.location + folderName + '/walletd.json', config.backup.location + folderName + '/walletd.log', config.backup.location + folderName + '/config.yml', config.backup.location + folderName + '/faucet.log', config.backup.location + folderName + '/config.json'];
-
-  fileArray.forEach(function(file) {
-    console.log('file:' + file);
-    const sha256value = sha256sum(file);
-    sha256value.then(function() {
-
-      console.log('sha256value: ' + sha256value);
-
-      sha256Array.push({
-        key: file,
-        value: sha256value,
-      });
-      console.log(config.backup.location + folderName + '/' + file);
-    });
-  });
-  fs.readdirSync(config.backup.location + folderName).forEach(file => {
-    // get sha for each file in config.backup.botConfigFile, config.backup.location + folderName dir
-    console.log('file: ' + config.backup.location + folderName + '/' + file);
-    const sha256value = sha256sum(config.backup.location + folderName + '/' + file);
-    sha256value.then(function() {
-
-      console.log('sha256value: ' + sha256value);
-
-      sha256Array.push({
-        key: config.backup.location + folderName + '/' + file,
-        value: sha256value,
-      });
-      console.log(config.backup.location + folderName + '/' + file);
-    });
-  });
-*/
   console.log('sha256Array: ' + JSON.stringify(sha256Array));
 
   // write the sha256 info to file
--- _scripts/backup/backup.sh
+++ _scripts/backup/backup.sh
@@ -8,18 +8,12 @@
 
 # Run this script from crontab just after the node backup file. 
 # This will tar, and move the files to specific directories for syncing to additional services. 
-# Requires the script to be in the users /home directory under the original name.
-# Also assumes that you are using the ./_scripts/backup/qrl-tipbotBackup/ directory in 
-# your config file. {config.backup.location}
 # get latest files into dir
 /usr/bin/nodejs $HOME/qrl-tips/_scripts/backup/backup.js
 
-FileLocation=`cat /home/$USER/qrl-tips/_config/config.json |jq -r .backup.location`
-echo $FileLocation
+FileLocation=`cat $HOME/qrl-tips/_config/config.json |jq -r .backup.location`
 
 cd $FileLocation
 # Tar the files to location defined in BackupLocation
 tar -czf TipBot_Backup.tar.gz -C $FileLocation/backup . >/dev/null 2>&1
 
-# Remove the old files to do this again later
-#rm -rf ${FileLocation}* 
