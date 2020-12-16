0537f0115bbd3ac674aabb89c3fdafced30a911b
fr1t2 <fr1t2@protonmail.com>
2020-12-16 11:38:31 -0700

    message response

--- _scripts/discord/cmd/help.js
+++ _scripts/discord/cmd/help.js
@@ -11,8 +11,31 @@ module.exports = {
     const messagedata = [];
     const { commands } = message.client;
 
+    // ReplyMessage(' Check your DM\'s');
+    function ReplyMessage(content) {
+      message.channel.startTyping();
+      setTimeout(function() {
+        message.reply(content);
+        message.channel.stopTyping(true);
+      }, 1000);
+    }
+
+    // errorMessage({ error: 'Can\'t access faucet from DM!', description: 'Please try again from the main chat, this function will only work there.' });
+    function errorMessage(content, footer = '  .: Tipbot provided by The QRL Contributors :.') {
+      message.channel.startTyping();
+      setTimeout(function() {
+        const embed = new Discord.MessageEmbed()
+          .setColor(0x000000)
+          .setTitle(':warning:  ERROR: ' + content.error)
+          .setDescription(content.description)
+          .setFooter(footer);
+        message.reply({ embed });
+        message.channel.stopTyping(true);
+      }, 1000);
+    }
+
     if (!args.length) {
-      messagedata.push('Here are all of my commands.\n*If you need more help try*: `+help {COMMAND}`\n```diff\n');
+      messagedata.push('Here are all of my commands.\n*If you need more help try:* `+help {COMMAND}`\n```diff\n');
       messagedata.push(commands.map(command => config.discord.prefix + command.name + ' - ' + command.description).join('\n'));
       messagedata.push('```');
       return message.reply(messagedata);
@@ -21,7 +44,9 @@ module.exports = {
     const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
     if (!command) {
-      return message.reply('that\'s not a valid command!');
+      errorMessage({ error: 'Not a valid command...', description: 'You have entered an invalid command for help'})
+      // message.reply('that\'s not a valid command!');
+      return;
     }
 
     data.push(`\n**Name:** ${command.name}`);
@@ -31,7 +56,7 @@ module.exports = {
     if (command.usage) data.push(`**Usage:** ${config.discord.prefix}${command.name} ${command.usage}`);
 
     data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-    message.channel.send(data, { split: true });
+    ReplyMessage(data, { split: true });
+    // message.channel.send(data, { split: true });
   },
 };
