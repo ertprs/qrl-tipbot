2b86b64490316b966c939f1ff94930676ba26b5e
fr1t2 <fr1t2@protonmail.com>
2020-12-15 23:36:42 -0700

    fix getuserinfo

--- _scripts/discord/cmd/optin.js
+++ _scripts/discord/cmd/optin.js
@@ -113,8 +113,9 @@ module.exports = {
 
         // user passed checks, opt them back in and check for future tips
         console.log('checks passed');
+        const user_id = user_info[0].user_id;
+        const oi = await optIn(user_id);
 
-        const oi = await optIn(user_info[0].user_id);
         future_tip_amount = user_info[0].future_tip_amount;
         if (future_tip_amount > 0) {
 
