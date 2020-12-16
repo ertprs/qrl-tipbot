9f0264f464a4fad5bdba14792029e7a1a4028e81
fr1t2 <fr1t2@protonmail.com>
2020-12-16 13:06:36 -0700

    message response

--- _scripts/discord/index.js
+++ _scripts/discord/index.js
@@ -269,9 +269,8 @@ client.on('message', message => {
   const now1 = new Date(now * 1000);
   
   const day = now1.getDate();
-  const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-  const month = months[now1.getMonth()];
-  const year = now1.getFullYear();
+  const month = now.getMonth();
+  const year = now.getFullYear();
   const hours = now1.getHours();
   const minutes = "0" + now1.getMinutes();
   const seconds = "0" + now1.getSeconds();
