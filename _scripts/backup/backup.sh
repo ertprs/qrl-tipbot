3204807793a2b7caa15447a652fbee7539d7dfa9
fr1t2 <fr1t2@protonmail.com>
2020-12-18 17:31:59 -0700

    Update backup locations

--- _scripts/backup/backup.sh
+++ _scripts/backup/backup.sh
@@ -12,16 +12,16 @@
 # places backup tar file in $HOME/qrl-tips/_scripts/backup/qrl-tipbotBackup/
 
 # get latest files into dir 
-/usr/bin/nodejs $HOME/qrl-tips/_scripts/backup/backup.js
+/usr/bin/nodejs $HOME/qrl-tipbot/_scripts/backup/backup.js
 # location defined in config file
-FileLocation=`cat $HOME/qrl-tips/_config/config.json |jq -r .backup.location`
+FileLocation=`cat $HOME/qrl-tipbot/_config/config.json |jq -r .backup.location`
 cd $FileLocation
 # Tar the files to location defined in BackupLocation
 tar -czf TipBot_Backup.tar.gz -C $FileLocation/backup . >/dev/null 2>&1
 
 openssl enc -pbkdf2 -e -base64 \
         -in TipBot_Backup.tar.gz -out TipBot_Backup.tar.gz.enc \
-        -pass file:$HOME/qrl-tips/_scripts/backup/qrl-tipbotBackup/secret_pass.txt
+        -pass file:$HOME/qrl-tipbot/_scripts/backup/qrl-tipbotBackup/secret_pass.txt
 
 # decrypt with 
 #openssl enc -pbkdf2 -d -base64 -out hey_TipBot_Backup.tar.gz -in TipBot_Backup.tar.gz.enc -pass file:$HOME/qrl-tips/_scripts/backup/qrl-tipbotBackup/secret_pass.txt
