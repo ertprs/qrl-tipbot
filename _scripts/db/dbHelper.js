b608c9d531f64123d66b15ef512c7e4bd80bc547
fr1t2 <fr1t2@protonmail.com>
2020-12-24 13:24:03 -0700

    check pending txns and clear if found confirmed

--- _scripts/db/dbHelper.js
+++ _scripts/db/dbHelper.js
@@ -432,11 +432,11 @@ async function CheckPendingTx(args) {
         wallet.GetTxInfo(pending.tx_hash).then(function(results) {
           //console.log('results: ' + JSON.parse(JSON.stringify(results)));
           const out = JSON.parse(results)
-          console.log('out: ' + out.confirmations)
+          console.log('out: ' + out)
           if (out.confirmations > 0) {
-            console.log('tx confirmed' + out.tx_hash);
+            console.log('tx confirmed' + result.tx_hash);
 
-            const dbInfo = 'UPDATE transactions SET pending = "0" WHERE tx_hash = "' + out.tx_hash + '"';          
+            const dbInfo = 'UPDATE transactions SET pending = "0" WHERE tx_hash = "' + result.tx_hash + '"';          
             console.log(dbInfo)
             callmysql.query(dbInfo, function(err, result) {
               console.log(JSON.stringify(result))
