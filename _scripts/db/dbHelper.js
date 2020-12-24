ee4b01960f3436030b45e33e7ee859e887a4c07f
fr1t2 <fr1t2@protonmail.com>
2020-12-24 11:37:42 -0700

    adding pending balance check

--- _scripts/db/dbHelper.js
+++ _scripts/db/dbHelper.js
@@ -83,14 +83,22 @@ async function GetAllUserInfo(args) {
 
       // update the balance in the wallet database and refresh info
       GetUserWalletBal({ user_id: user_id }).then(function(balance) {
+        //check for pending tx's
+        CheckPendingTx({ user_id: user_id }).then(function(pendingBal) {
+          console.log('pendign BAl Request: ' + JSON.stringify(pendingBal));
+
         const bal = JSON.stringify(balance);
+        // const pbal = JSON.stringify(pendingBal);
+
         const wallet_bal = balance.wallet_bal;
+        // const pending_bal = pbal.pending
         // console.log('balance: ' + wallet_bal);
         foundResArray.push({ user_found: has_user_found, user_agree: has_user_agree, opt_out: has_opt_out, wallet_pub: wallet_pub, wallet_bal: wallet_bal, user_id: U_id, user_name: user_name, optout_date: optout_date });
         // Array.prototype.push.apply(foundResArray, infoResult);
         // console.log('getAllInfoSearch foundResArray ' + JSON.stringify(foundResArray) + '\n');
         resolve(foundResArray);
         return foundResArray;
+        });
       });
 
       /* // check for user agree results
