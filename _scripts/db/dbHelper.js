949d01628de13cc98413637a970cd64214931e57
fr1t2 <fr1t2@protonmail.com>
2020-12-24 12:14:21 -0700

    for pending array

--- _scripts/db/dbHelper.js
+++ _scripts/db/dbHelper.js
@@ -425,7 +425,7 @@ async function CheckPendingTx(args) {
       console.log('searchResults:' + JSON.stringify(result));
       console.log('result.length:' + JSON.stringify(result.length));
 
- for (var i = 0; ; i < JSON.stringify(result.length); i++) {
+ for (var i = 0; ; i < result.length; i++) {
         console.log('i:' + i);
         var pending = result.[i];
         for (var j = 0; j < pending.length; j++) {
