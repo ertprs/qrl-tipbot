a9ee768c6ae815a352ddcf99d6276f22fbfb78d3
fr1t2 <fr1t2@protonmail.com>
2020-12-27 23:10:52 -0700

    Rewrite withdraw and add full check logic

--- _scripts/discord/cmd/withdraw.js
+++ _scripts/discord/cmd/withdraw.js
@@ -136,7 +136,7 @@ module.exports = {
       return new Promise(resolve => {
         // {tip_id: fromTipDB, tx_hash: fromTX_HASH}
         console.log('transactionsDbWrite args:' + JSON.stringify(txArgs));
-        const txInfo = { tip_id: txArgs.tip_id, tx_hash: txArgs.tx_hash, tx_type: withdraw };
+        const txInfo = { tip_id: txArgs.tip_id, tx_hash: txArgs.tx_hash, tx_type: 'withdraw' };
         const wdTxEntry = dbHelper.addTransaction(txInfo);
         resolve(wdTxEntry);
       });
