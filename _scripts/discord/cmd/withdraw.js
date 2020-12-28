c8431d6ac94a31b16c7aa63a77f4abde404464a6
fr1t2 <fr1t2@protonmail.com>
2020-12-27 22:37:03 -0700

    Rewrite withdraw and add full check logic

--- _scripts/discord/cmd/withdraw.js
+++ _scripts/discord/cmd/withdraw.js
@@ -304,6 +304,7 @@ module.exports = {
       // check passed, do stuff
 
       const transferInfo = { address_to: check[0].addressArray, amount: check[0].amtArray, fee: fee, address_from: userArray[0].wallet_pub };
+      console.log('transferInfo: ' + transferInfo)
       const transferFunds = await sendFunds(transferInfo);
       console.log('transferFunds: ' + JSON.stringify(transferFunds));
 
