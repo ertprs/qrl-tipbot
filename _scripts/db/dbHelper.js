5877f5bff2e9c534e5c3d399258a9b26da280b37
fr1t2 <fr1t2@protonmail.com>
2020-12-28 01:03:47 -0700

    logging

--- _scripts/db/dbHelper.js
+++ _scripts/db/dbHelper.js
@@ -459,7 +459,7 @@ async function CheckPendingTx(args) {
     const id = input.user_id;
     // const resultArray = [];
     const searchDB = 'SELECT tips.from_user_id AS discord_user, tips.tip_amount AS tip_amount, tips.id AS tip_id, tips.time_stamp AS tip_timestamp, transactions.pending AS pending, transactions.tx_hash AS tx_hash FROM tips, transactions WHERE transactions.pending = "1" AND tips.from_user_id =  "' + id + '" AND transactions.tip_id = tips.id';
-    // console.log('serchDB: ' + searchDB);
+    console.log('serchDB: ' + searchDB);
     callmysql.query(searchDB, function(err, result) {
       if (err) {
         console.log('[mysql error]', err);
