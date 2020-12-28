d0cbdb1498f7857988d55de3b9a0922ec533f2fc
fr1t2 <fr1t2@protonmail.com>
2020-12-27 22:49:43 -0700

    Rewrite withdraw and add full check logic

--- _scripts/discord/cmd/withdraw.js
+++ _scripts/discord/cmd/withdraw.js
@@ -310,11 +310,11 @@ module.exports = {
       const transferFunds = await sendFunds(transferInfo);
       const transferFundsOut = JSON.parse(transferFunds);
       console.log('transferFunds: ' + JSON.stringify(transferFundsOut));
-      const wdDbWrite = await withdrawDBWrite({ user_id: check[0].userArray[0][0].user_id, tx_hash: transferFunds.tx.transaction_hash, to_address: check[0].addressArray, amt: check[0].amtArray });
+      const wdDbWrite = await withdrawDBWrite({ user_id: check[0].userArray[0][0].user_id, tx_hash: transferFundsOut.tx.transaction_hash, to_address: check[0].addressArray, amt: check[0].amtArray });
       console.log('wdDbWrite: ' + JSON.stringify(wdDbWrite));
 
 
-      const txDbWrite = await transactionsDBWrite({ tip_id: wdDbWrite.insertId, tx_hash: transferFunds.tx.transaction_hash });
+      const txDbWrite = await transactionsDBWrite({ tip_id: wdDbWrite.insertId, tx_hash: transferFundsOut.tx.transaction_hash });
       console.log('txDbWrite: ' + JSON.stringify(txDbWrite));
 
 
