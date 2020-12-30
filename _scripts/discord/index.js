cefb81d68a93c712d73d572af46a6e654e72f9b1
fr1t2 <fr1t2@protonmail.com>
2020-12-29 17:03:21 -0700

    bot_details

--- _scripts/discord/index.js
+++ _scripts/discord/index.js
@@ -263,8 +263,8 @@ client.on('message', message => {
     const messageGuild = message.guild;
     console.log('message.guild.members: ' + JSON.stringify(messageGuild.members));
     console.log('message.guild.members.length: ' + (message.guild.members).length);
-    for(let i = 0, l = (message.guild.members).length; i < l; i++) {
-      console.log('user: ' + message.guild.members[i]);
+    for(let i = 0, l = (messageGuild.guild.members).length; i < l; i++) {
+      console.log('user: ' + messageGuild.guild.members[i]);
     }
     // const channels = await client.channels.cache.get(config.bot_details.ban_channel_id);
     // console.log('channels: ' + JSON.stringify(channels));
