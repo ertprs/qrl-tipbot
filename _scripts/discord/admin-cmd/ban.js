950e03df4f26ec9b2f7818aea45a3318acad5b13
fr1t2 <fr1t2@protonmail.com>
2020-12-30 08:49:04 -0700

    bancommand

--- _scripts/discord/admin-cmd/ban.js
+++ _scripts/discord/admin-cmd/ban.js
@@ -116,9 +116,10 @@ module.exports = {
             .setTitle('**TipBot Secret Keys**')
             .setDescription('You have been banned from the server. Because of this you cannot use this tipbot service.\n\n \
                 Below are the private keys to the tipbot account you held, with a positive balance of `' + toQuanta(wallet_bal) + ' QRL`.\n \
-                Please use the [QRL Web Wallet](' + config.wallet.wallet_url + ') to withdraw these funds into an address you own.\n\n \
+                Please use the [QRL Web Wallet](' + config.wallet.wallet_url + ') to withdraw these funds into an address you own. \
+                After one week the funds may be re-claimed by the service and forfeited by the user. \n\n \
                 :exclamation: **There is NO support offered**\n \
-                :exclamation: **ANY FUNDS LEFT IN THE ADDRESS ARE SUBJECT TO LOSS** After one week the funds may be re-claimed by the service and forfeited by the user. \n \
+                :exclamation: **ANY FUNDS LEFT IN THE ADDRESS ARE SUBJECT TO LOSS**\n \
                 :exclamation: **This is the last message you will receive from the tipbot** :exclamation: ')
             .addField('**__WARNING: Protect These Keys!__**', ' ***NEVER SHARE THESE KEYS WITH ANYONE FOR ANY REASON!!***')
             .addField('Public Address: ', '`' + walletPub + '`')
