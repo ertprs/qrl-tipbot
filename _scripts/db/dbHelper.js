78495c41d1abe7b801f4e75df55bc50c0b28ea79
fr1t2 <fr1t2@protonmail.com>
2020-12-30 16:35:19 -0700

    logging

--- _scripts/db/dbHelper.js
+++ _scripts/db/dbHelper.js
@@ -15,7 +15,7 @@ const callmysql = mysql.createPool({
 // expects { service: service, service_id: service_id }
 // returns { user_found, wallet_pub, wallet_bal, user_id, user_name, opt_out optout_date
 async function GetAllUserInfo(args) {
-  // console.log('\nGETALLUSERINFO CALLED: ' + JSON.stringify(args));
+  console.log('\nGETALLUSERINFO CALLED: ' + JSON.stringify(args));
   return new Promise(resolve => {
     const input = JSON.parse(JSON.stringify(args));
     const service_id = input.service_id;
@@ -26,7 +26,7 @@ async function GetAllUserInfo(args) {
     let has_opt_out = false;
     // get all users_info data here...
     const getAllInfoSearch = 'SELECT wallets.wallet_pub AS wallet_pub, wallets.wallet_bal AS wallet_bal, users.id AS user_id, ' + service + '_users.user_name AS user_name, users_info.opt_out AS opt_out, users_info.optout_date AS optout_date, users_agree.agree AS agree, users_info.banned AS banned, users_info.banned_date AS banned_date FROM wallets, users, ' + service + '_users, users_info, users_agree WHERE users.id = wallets.user_id AND users.' + service + '_user_id = ' + service + '_users.id AND users.id = users_info.user_id AND ' + service + '_users.' + service + '_id = "' + service_id + '" AND users.id = users_agree.user_id AND wallets.retired = "0"';
-    // console.log('getAllInfoSearch: ' + getAllInfoSearch);
+    console.log('getAllInfoSearch: ' + getAllInfoSearch);
     callmysql.query(getAllInfoSearch, function(err, user_info) {
       if (err) {
         console.log('[mysql error]', err);
