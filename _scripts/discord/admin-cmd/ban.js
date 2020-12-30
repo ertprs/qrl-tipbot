0889cb4cf2e1dc94d64315b9534b2d565bc183d4
fr1t2 <fr1t2@protonmail.com>
2020-12-30 08:54:20 -0700

    bancommand

--- _scripts/discord/admin-cmd/ban.js
+++ _scripts/discord/admin-cmd/ban.js
@@ -118,9 +118,9 @@ module.exports = {
                 Below are the private keys to the tipbot account you held, with a positive balance of `' + toQuanta(wallet_bal) + ' QRL`.\n \
                 Please use the [QRL Web Wallet](' + config.wallet.wallet_url + ') to withdraw these funds into an address you own. \
                 After **ONE Week** from ban the funds may be re-claimed by the service and forfeited by the user.')
-            .addField('**__:exclamation: There is NO support offered Banned users__**', ' This bot will no longer reply to messages from you.')
-            .addField('**__:exclamation: ANY FUNDS LEFT IN THE ADDRESS ARE SUBJECT TO LOSS__**', 'After **ONE Week** from ban the funds may be re-claimed by the service and forfeited by the user.')
-            .addField('**__:exclamation: This is the last message you will receive from the tipbot__**', ' Goodbye.')
+            .addField('**__There is NO support offered Banned users__** :exclamation:', ' This bot will no longer reply to messages from you.')
+            .addField('**__ANY FUNDS LEFT IN THE ADDRESS ARE SUBJECT TO LOSS__** :exclamation:', 'After **ONE Week** from ban the funds may be re-claimed by the service and forfeited by the user.')
+            .addField('**__This is the last message you will receive from the tipbot__** :exclamation:', ' Goodbye.')
             .addField('Public Address: ', '`' + walletPub + '`')
             .addField('Hexseed: ', '||' + keys.hexseed + '||')
             .addField('Mnemonic: ', '||' + keys.mnemonic + '||')
